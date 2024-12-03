c225d02a426f03762e69f4bc4d5cd1cc1080e676
Pongsatornkerdsatorn <pongsatorn.ker@spumail.net>
2024-12-03 16:15:30 +0700

    all

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    fs: {
+      strict: false, // อนุญาตให้เข้าถึงไฟล์ในระบบ
+    },
+  },
+  plugins: [react()],
+  base: '/project-slide-me/',
+})
+
+
